cd18d8659f1a6736d55ad018113c6190ae66154b
VitorCostaVianna <vcvianna@sga.pucminas.br>
2026-01-06 21:37:58 -0300

    first commit

---
+++ _legacy_backup/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PointDogPet</title>
+    <script src="https://kit.fontawesome.com/1a985785a6.js" crossorigin="anonymous"></script>  <script src="https://kit.fontawesome.com/1a985785a6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="CSS/main.css">
+</head>
+<body>
+    <div class="container">
+
+        <header>
+            <div class="logo">
+              <a href="index.php"><img src="img/logo-pointdog.png" alt="logo-pointdog"></a> 
+            </div>
+            <p> Cuidando de Pets Desde 1998 <i class="fa-solid fa-dog"></i></p>
+        </header>
+        <main>
+           
+                <div class="menurede"> 
+                    <div class="bebas-neue-regular" >
+                        <h1>Acompanhe nossas <br><span> redes sociais </span></h1>
+                    </div>
+                        <div class="redeSocial">
+                            <ul>
+                                <li><a href="https://www.instagram.com/pointdogpet"><i class="fa-brands fa-square-instagram"></i> Instagram - PointDogPet</a></li>
+                                <li><a href="https://www.instagram.com/_pettmix_/"><i class="fa-brands fa-square-instagram"></i> Instagram - PetMix</a></li>
+                                <li><a href="https://www.facebook.com/pointdogpet"><i class="fa-brands fa-facebook"></i> facebook - PointDogPet</a></li>
+                                <li><a href="https://linktr.ee/pointdogpetshop"><i class="fa-solid fa-tree"></i> Linktree</a></li>
+                            </ul>
+                            <img src="img/ImagemDOGm.svg" alt="">
+                        </div>   
+                </div>
+                
+
+                <div class="historia">
+                    <div class="bebas-neue-regular">
+                        <h1> 
+                            Saiba mais da nossa história 
+                        </h1>
+                    </div>
+                    <p>
+                        No mercado Pet a mais de <b> 26 anos </b> a <b>Point Dog</b> vem trilhando um caminho de sucesso e muito trabalho.
+                        <br><br>
+                        Hoje a marca se tornou referência em <b>Belo Horizonte</b>, pioneira no ramo de banho e tosa com agendamento de forma automatica, demonstrando seriedade e respeito aos clientes.
+                        <br><br>
+                        Contamos com colaboradores altamente qualificados e capacitados para oferecer um serviço de qualidade e proporcionar aos nossos clientes um melhor atendimento e confiança.
+                    </p>
+                    <img src="img/NancyAntiga.svg" alt="">
+                </div>
+            </main>
+            <aside>
+                    <div class="bebas-neue-regular">
+                        <h1> Conheça nossas <br> <span class="unidade"> unidades </span></h1>
+                    </div>
+                    <div class="menuUnidades">
+                        <ul>
+                            <li></i><a href="horto.html"><i class="fa-solid fa-arrow-right"></i>         Unidade Horto</a> <br> Rua Dona Nancy de Vasc. Gomes,336 - Horto</li>
+                            <li><a href="santaT.html"><i class="fa-solid fa-arrow-right"></i>         Unidade Santa Tereza</a> <br> Rua Formosa, 113A - Santa Tereza</li>
+                            <li><a href="pitangui.html"><i class="fa-solid fa-arrow-right"></i>         Unidade Sagrada Família/Horto</a> <br> Rua Pitangui, 3003 - Sagrada Família</li>
+                            <li><a href="saoroque.html"><i class="fa-solid fa-arrow-right"> </i>            Unidade Sagrada Família</a> <br> Rua São Roque, 113A - Sagrada Família</li>
+                        </ul>
+                    </div>
+                    <img src="img/Horariomaior.svg" alt="Imagem com o Horário de funcionamento" >
+            </aside>
+        <footer>
+            <p>
+                Copiright 2024 <i class="fa-solid fa-paw"></i> PointDogPet <i class="fa-solid fa-paw"></i> Todos os direitos reservados <i class="fa-solid fa-shield-dog"></i>
+            </p>
+        </footer>
+    </div>
+    
+</body>
+</html>
